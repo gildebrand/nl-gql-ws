963255be4656a61569089c3855ae983e0e48a082
Anton Gildebrand <anton.gildebrand@eqtpartners.com>
2023-03-15 18:23:20 +0100

    WIP

--- backend/src/db/TodoDB.ts
+++ backend/src/db/TodoDB.ts
@@ -1,20 +1,38 @@
-import { PrismaClient, Todo } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 export const TodoDB = {
   getTodos({
-    authorId = null
+    authorId = null,
+    done = null,
   }: {
     authorId?: number | null;
+    done?: boolean | null;
   } = {}) {
     return prisma.todo.findMany({
       where: {
-        ...(authorId !== null && {authorId})
+        ...(authorId !== null && {authorId}),
+        ...(done !== null && {done}),
       }
     });
   },
-  createTodo(todo: Todo) {
-    return prisma.todo.create({data: todo});
+  createTodo(data: Prisma.TodoCreateInput) {
+    return prisma.todo.create({data});
+  },
+  updateTodo(id: number, data: Prisma.TodoUpdateInput) {
+    return prisma.todo.update({
+      where: {
+        id
+      },
+      data,
+    });
+  },
+  removeTodo(id: number) {
+    return prisma.todo.delete({
+      where: {
+        id
+      }
+    });
   }
 } as const;
